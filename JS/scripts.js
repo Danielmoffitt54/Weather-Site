86ea1c838be9d484694f56622adb9ab23c6c6608
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-10-21 14:22:26 -0500

    Set up the background image for the cards to be dynamic, and added in the collapsing portion of the card to show the weekly forecast.

--- JS/scripts.js
+++ JS/scripts.js
@@ -108,21 +108,55 @@ function getWeatherInfo(latitude, longitude, city, region) {
             // ^ Get the HTML from the div with the ID template.
 
             let temperature = data.currently.temperature;
+            let weatherIcon = data.currently.icon;
             let currently = data.currently.summary;
             let tempHigh = data.daily.data[0].temperatureHigh;
             let tempLow = data.daily.data[0].temperatureLow;
-            let precipiProb = data.currently.precipProbability;
+            let windSpeed = data.currently.windSpeed;
+            let windGust = data.currently.windGust;
+            let windBearing = data.currently.windBearing;
             let humidity = data.currently.humidity;
             // ^ Get variables from the dark sky data.
 
+            let color = getFontColor(weatherIcon);
+            let windDirection = getWindDirection(windBearing);
+
             templateHTML = templateHTML.replace("@@city@@", city + ", " + region);
             templateHTML = templateHTML.replace("@@temperature@@", temperature + "&#8457");
+            templateHTML = templateHTML.replace("@@imageURL@@", "../IMG/" + weatherIcon + ".jpg");
             templateHTML = templateHTML.replace("@@currently@@", "Currently: " + currently);
-            templateHTML = templateHTML.replace("@@high@@", "High Temp: " + tempHigh + "&#8457");
-            templateHTML = templateHTML.replace("@@low@@", "Low Temp: " + tempLow + "&#8457");
-            templateHTML = templateHTML.replace("@@precipiProb@@", "Precipitaion Probability: " + precipiProb + "%");
+            templateHTML = templateHTML.replace("@@high@@", "High Temp: " + Math.round(tempHigh));
+            templateHTML = templateHTML.replace("@@low@@", "Low Temp: " + Math.round(tempLow));
+            templateHTML = templateHTML.replace("@@wind@@", "Wind: " + windDirection + " " + Math.round(windSpeed) + " MPH, Gust: " + Math.round(windGust) + " MPH");
             templateHTML = templateHTML.replace("@@humidity@@", "Humidity: " + humidity);
             // ^ Replace the string "@@value@@" with the Dark sky data we pass into this function in the HTML.
+
+            for (var i = 0; i < 5; i++) {
+                let dailyData = data.daily.data[i];
+
+
+                if (i > 0) {
+                    let date = new Date();
+                    date.setDate(date.getDate() + i);
+                    // ^ Get Dates for each day
+
+                    let month = date.getMonth() + 1;
+                    let day = date.getDay();
+                    
+                    templateHTML = templateHTML.replace("@@date" + i + "@@", month + "/" + day);
+                }
+                // ^ Set the Date
+
+                dailySummary = dailyData.summary;
+                dailyHigh = dailyData.temperatureMax;
+                dailyLow = dailyData.temperatureMin;
+                dailyPrecip = dailyData.precipProbability;
+
+                templateHTML = templateHTML.replace("@@summary" + i + "@@", dailySummary);
+                templateHTML = templateHTML.replace("@@max" + i + "@@", Math.round(dailyHigh));
+                templateHTML = templateHTML.replace("@@low" + i + "@@", Math.round(dailyLow));
+                templateHTML = templateHTML.replace("@@precip" + i + "@@", Math.round(dailyPrecip * 100));
+            }
 
             $(".row").append(templateHTML);
             // ^ Add the configured template HTML to our row in the card container.
@@ -136,3 +170,43 @@ function getWeatherInfo(latitude, longitude, city, region) {
     });
 }
 // ^ Function to connect to the Dark Sky API and get weather data.
+
+function getFontColor(weatherIcon) {
+    switch (weatherIcon) {
+        case "clear-day":
+            return "../IMG/clear-day.jpg";
+        case "clear-night":
+            return "../IMG/clear-night.jpg";
+        case "rain":
+            return "../IMG/rain.jpg";
+        case "snow":
+            return "../IMG/snow.jpg";
+        case "sleat":
+            return "../IMG/sleat.jpg";
+        case "wind":
+            return "../IMG/wind.jpg";
+        case "fog":
+            return "../IMG/fog.jpg";
+        case "cloudy":
+            return "../IMG/cloudy.jpg";
+        case "partly-cloudy-day":
+            return "../IMG/partly-cloudy-day.jpg";
+        case "partly-cloudy-night":
+            return "../IMG/partly-cloudy-night.jpg";
+        default:
+            return "../IMG/clear-day.jpg";
+    }
+}
+
+function getWindDirection(windBearing) {
+    switch (true) {
+        case (windBearing > 315 || windBearing < 45):
+            return "N";
+        case (windBearing >= 45 && windBearing <= 135):
+            return "E";
+        case (windBearing > 135 && windBearing < 225):
+            return "S";
+        case (windBearing >= 225 && windBearing <= 315):
+            return "W";
+    }
+}
